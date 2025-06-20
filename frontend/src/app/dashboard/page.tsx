72d59b29505aab9290193a675552ccc427072ce2
Abhiram86 <abhiramalla86@gmail.com>
2025-06-20 22:15:36 +0530

    frontend backend connection

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -12,20 +12,24 @@ export default function Dashboard() {
   if (!user) router.replace("/login");
 
   const [data, setData] = useState<string | null>(null);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     const getData = async () => {
       const res = await getDashboard();
       if (!res) return;
       setData(res.message);
+      setLoading(false);
     };
     getData();
-  }, []);
+  }, [user]);
   return (
-    <div className="flex flex-col text-3xl items-center text-center mt-20">
-      {data ? (
+    <div className="flex flex-col text-3xl items-center text-center mt-20 min-h-[200px]">
+      {loading ? (
+        <div className="w-64 h-8 bg-gray-300 animate-pulse rounded"></div>
+      ) : data ? (
         <p>{data}</p>
       ) : (
-        <p>you need to be logged in to access this route</p>
+        <p>You need to be logged in to access this route</p>
       )}
     </div>
   );
